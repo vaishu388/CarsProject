9b723f27b8fc70ae9b4365436e00ac60a73adb89
vaishu388 <un.escorted123@gmail.com>
2024-07-09 23:53:25 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
+
+
 app.use(express.json())
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
